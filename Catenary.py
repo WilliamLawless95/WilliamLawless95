8f261e10a96828dafaec809d5171f93254b424a2
WilliamLawless95 <91984756+WilliamLawless95@users.noreply.github.com>
2022-04-27 09:24:43 -0400

    Add files via upload
    This Python script solves the 'Hanging Cable Problem', allegedly featured as an interview question at Amazon. Given a cable of 80m in length, suspended between two towers of 50m in height, hanging 20m above the ground at its lowest point, the candidate is asked for the distance between the two poles. The question is repeated with the cable hanging only 10m above the ground. (Hint: at 10m, the cable is neatly folded in two as the towers are at the same point.) 
    
    This script prompts the user to enter the following data:
    1. The length of the cable (in meters),
    2. The height of the first tower (in meters),
    3. The height of the second tower (in meters), and
    4. The height of the cable at its lowest point (in meters). If the towers are of unequal height, then enter 0.
    
    If the towers are of equal height, then we use the Hyperbolic Identity (cosh**2(t) - sinh**2(t) = 1) to determine the distance between the towers. If the towers are of unequal height, then the math gets a lot uglier, eventually using the Quadratic Formula to determine the distance between the towers. All results are rounded to 3 decimal places.

---
+++ Catenary.py
@@ -0,0 +1,94 @@
+# This Python script solves the 'Hanging Cable Problem' featured as an Amazon interview question.
+
+# The script prompts the user to enter the length of a hanging cable,
+# the height of each of the two towers between which the cable hangs, and
+# the height of the cable at its lowest point.
+
+# Import the math library.
+import math
+
+# Import the name and system modules from the os library.
+from os import name, system
+
+# This function clears the screen for the user to see the output more clearly.
+
+
+def clear():
+    if name == 'nt':
+        _ = system('cls')
+    else:
+        _ = system('clear')
+
+
+# Prompt the user to enter the length of the cable (in meters).
+length = float(input('Please enter the length of the cable (m): '))
+
+# Prompt the user to enter the height of the first tower (in meters).
+tower1 = float(input('Please enter the height of the first tower (m): '))
+
+# Prompt the user to enter the height of the first tower (in meters).
+tower2 = float(input('Please enter the height of the second tower (m): '))
+
+# Prompt the user to enter the height of the center of the cable (in meters).
+# If the towers are of unequal heights, the user is to enter 0.
+height = float(input('Please enter the height of the cable\'s lowest point (m)\n - '
+                     '(If the towers are of unequal heights,enter 0): '))
+if tower1 != tower2:
+    height = 0
+print('\n')
+clear()
+
+# Move the cable into the coordinate plane with the tangential point (0, 0).
+# If the towers are of equal heights, then:
+if tower1 == tower2:
+    y = tower1 - height
+    x = length / 2
+
+# The Hyperbolic Identity is: cosh**2(t) - sinh**2(t) = 1
+    try:
+        a = abs((y**2 - x**2) / (2*y))
+        print('The cable is ', length, 'm long.')
+        print('The two towers are ', tower1, 'm high.')
+        print('The cable hangs ', height, 'm off the ground.\n')
+        x = math.asinh(x / a)
+    # The cable is folded in two.
+    except ZeroDivisionError:
+        print('The two towers are at the same point.')
+    else:
+        if height < 0:
+
+            # The cable has too much slack.
+            print('ERROR: The cable is dragging on the ground.')
+        else:
+
+            # Print out the results, rounding to 3 decimal places.
+            print('The distance between the two towers is {:.3f}'.format(2 * a * x), 'm.')
+else:
+    # If the two towers are of unequal heights, then the math gets a lot uglier.
+    z1 = tower1
+    z2 = tower2
+    y = length
+    print('The entire cable is ', y, 'm long.')
+    print('The first tower is ', z1, 'm high.')
+    print('The second tower is ', z2, 'm high.')
+
+    # Solve for a using the Quadratic Equation.
+    a1 = (((z1 + z2) * (y ** 2 - (z1 - z2) ** 2)) +
+          (2 * y * math.sqrt(z1 * z2 * (y ** 2 - (z1 - z2) ** 2)))) / (2 * (z1 - z2) ** 2)
+    a2 = (((z1 + z2) * (y ** 2 - (z1 - z2) ** 2)) -
+          (2 * y * math.sqrt(z1 * z2 * (y ** 2 - (z1 - z2) ** 2)))) / (2 * (z1 - z2) ** 2)
+
+    # We use the small of the two solutions.
+    a = min(a1, a2)
+
+    # We calculate the length of each cable segment such that y1 + y2 = y.
+    y1 = math.sqrt(z1 ** 2 + 2 * z1 * a)
+    y2 = math.sqrt(z2 ** 2 + 2 * z2 * a)
+
+    # We calculate the between the two towers such that x1 + x2 = x.
+    x1 = a * math.log((z1 + y1 + a) / a)
+    x2 = a * math.log((z2 + y2 + a) / a)
+
+    # Print out the results, rounding to 3 decimal places.
+    print('The two cable segments are {:.3f}'.format(y1), ' m and {:.3f}'.format(y2), ' m long, respectively.')
+    print('The two towers are {:.3f}'.format(x1 + x2), ' m apart.')
